c98618c6ee531511fe68d7460f41f8c8fef410d3
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-05-07 17:36:38 +0300

    Create SyrAutofarm.lua

---
+++ SyrAutofarm.lua
@@ -0,0 +1,23 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+--start
+local Window = OrionLib:MakeWindow({Name = "SyrHubProPsx", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+--stage 1 (tab 1)
+local AutoTab = Window:MakeTab({ 
+
+         Name = "Auto Farm", 
+
+         Icon = "rbxassetid://4483345998", 
+
+         PremiumOnly = false 
+
+ })
+ 
+ -- section
+ local FirstSection = AutoTab:AddSection({ 
+
+         Name = "Thank you for using, updates soon!" 
+
+ })
+ 
