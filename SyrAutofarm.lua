f24869bd39a51b3fb49e7069a6e3629e814a67c5
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-05-07 20:58:29 +0300

    Update SyrAutofarm.lua

--- SyrAutofarm.lua
+++ SyrAutofarm.lua
@@ -26,7 +26,10 @@ local AutoTab = Window:MakeTab({
 
  AutoTab:AddToggle({
 	Name = "Town",
-	Default = false,
+	Default = _G.AutoFarm = false
+  _G.Area = "Town"
+loadstring(game.HttpGet(game,"https://raw.githubusercontent.com/zuhnosu/psx-auto-farm/main/main.lua", true))()
+	end ,
 	Callback = function(Value)
 		_G.AutoFarm = true
   _G.Area = "Town"
